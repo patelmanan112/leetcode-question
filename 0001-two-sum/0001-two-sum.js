89bbda09033de54e2c34f4a6e1fb5e9b73967985
Manan Patel <manan.patel.cg@gmail.com>
2026-04-09 09:46:51 +0530

    Time: 60 ms (12.71%), Space: 54.4 MB (78.31%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let arr = [];
+    for(let i=0; i<nums.length; i++){
+        for(let j=0; j<nums.length; j++){
+            if(nums[i] + nums[j] == target && i != j){
+                arr[0] = i;
+                arr[1] = j;
+                return arr
+            }
+        }
+    }
+};

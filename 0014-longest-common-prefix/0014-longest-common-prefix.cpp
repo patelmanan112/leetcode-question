8412b7d6327d58a84cfed30c9a3c3367d8170218
Manan Patel <manan.patel.cg@gmail.com>
2026-04-14 13:33:56 +0530

    Time: 0 ms (100%), Space: 11.8 MB (62.9%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string subString = strs[0];
+        
+        for(int i=1; i<strs.size() ; i++){
+            while(strs[i].find(subString) !=0){
+                subString.pop_back();
+            }
+        }
+        return subString;
+    }
+};

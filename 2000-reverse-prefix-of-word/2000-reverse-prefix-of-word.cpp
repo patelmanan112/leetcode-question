bea1b45498d13a9743e29b712282c7e7e57652dd
Manan Patel <manan.patel.cg@gmail.com>
2026-04-27 15:15:16 +0530

    Time: 0 ms (100%), Space: 8.4 MB (32.85%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-        int index =0;
-        for(int i=0 ; i < word.size() ; i++){
+        int index = 0;
+        for(int i=0; i<word.size() ; i++){
+
             if(word[i] == ch){
                 index = i;
                 break;
             }
         }
-    int i=0;
-    int j = index;
-    if(j == 0){
-        return word;
-    }
-    while(i<j){
-        char sha = word[i];
+        if(index ==0){
+            return word;
+        }
+    int i=0 ;
+    int j =index;
+    while( i<j){
+        char saag = word[i];
         word[i] = word[j];
-        word[j] = sha;
+        word[j] = saag;
         i++;
-        j--;
+        j--; 
     }
-    
+
     return word;
     }
 };

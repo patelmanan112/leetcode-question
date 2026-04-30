a5911cf20df2d07fdb8550cadedf7630cc27d1f9
Manan Patel <manan.patel.cg@gmail.com>
2026-04-30 14:07:13 +0530

    Time: 0 ms (100%), Space: 8.6 MB (78.09%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int addDigits(int num) {
-        while( num >9){
-            int temp = num;
+        while( num> 9){
             int sum =0;
-            while( temp!=0){
+            int temp = num;
+            while(temp!=0){
                 int upd = temp%10;
-                sum = sum + upd;
+                sum += upd;
                 temp = temp/10;
             }
             num = sum;
+
         }
+
         return num;
     }
 };

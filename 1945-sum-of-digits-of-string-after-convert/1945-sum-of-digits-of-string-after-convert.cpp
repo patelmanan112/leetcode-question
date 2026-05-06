2bd96fdcb7e0a2325458cdba2ed5549205a82d31
Manan Patel <manan.patel.cg@gmail.com>
2026-05-06 16:03:57 +0530

    Time: 0 ms (100%), Space: 9.6 MB (39.36%) - LeetHub

--- 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int getLucky(string s, int k) {
-        string result;
-        for (int i = 0; i < s.size(); i++) {
-            int asc = s[i] - 'a' + 1; 
-            result += to_string(asc); 
+        string res;
+        for(int i=0; i<s.size() ; i++){
+            int asc = s[i] - 'a' + 1;
+            res += to_string(asc);
         }
-        int sum = 0;
-        for (int i = 0; i < k; i++) {
-            sum = 0; 
-            for (int j = 0; j < result.size(); j++) {
-                sum += result[j] - '0';
+        int sum =0;
+        for(int j =0; j<k; j++){
+            sum =0;
+            for(int k =0; k<res.size() ; k++){
+                sum += res[k] - '0';
             }
-
-            result = to_string(sum);   
+            res = to_string(sum);
         }
 
         return sum;
+
+        
     }
 };

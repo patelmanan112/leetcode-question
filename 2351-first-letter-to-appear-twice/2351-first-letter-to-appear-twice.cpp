1847b9a53f7544cef584bb9f44192718905baae4
Manan Patel <manan.patel.cg@gmail.com>
2026-05-06 15:47:23 +0530

    Time: 0 ms (100%), Space: 8.3 MB (70.7%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     char repeatedCharacter(string s) {
-        unordered_map<char ,int> mp;
-
+        unordered_map<char , int> mp;
         for(int i=0; i<s.size() ; i++){
             mp[s[i]]++;
-
             if(mp[s[i]] == 2){
                 return s[i];
             }
-
         }
 
-        return '\0';
+        return '/0';
     }
 };

7197f4a6acc6b7f4842960bfe7b58423444175d9
Manan Patel <manan.patel.cg@gmail.com>
2026-05-06 15:31:37 +0530

    Time: 0 ms (100%), Space: 8 MB (3.5%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     bool isSameAfterReversals(int num) {
-        int rev;
-        int temp = num;
-        while(temp!=0){
-            int upd = temp%10;
-            rev = rev*10 + upd;
-            temp = temp/10;
-        }
+  int reverednum =0;
+  int temp = num;
+  while(temp!=0){
+    int upd = temp%10;
+    reverednum = reverednum*10 + upd;
+    temp = temp/10;
 
-        return to_string(rev).size() == to_string(num).size() ? true : false;
+  }
+
+  return to_string(reverednum).size() == to_string(num).size() ? true : false;
     }
 };

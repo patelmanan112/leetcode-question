c2f6bea907119309a2e4596ddd2f5f424131b9c2
Manan Patel <manan.patel.cg@gmail.com>
2026-05-10 15:22:59 +0530

    Time: 3 ms (23%), Space: 12.8 MB (99.74%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int mostWordsFound(vector<string>& sentences) {
-       int count =0;
-       int maxiu =0;
-       for(int i=0; i<sentences.size() ; i++){
-        count =0;
-        for(int j =0; j <sentences[i].size() ; j++){
-            if(sentences[i][j] == ' '){
-                count++;
+        int count =0;
+        int maxium =0;
+        for(int i=0; i<sentences.size() ; i++){
+            count =0;
+            for(int j=0; j<sentences[i].size() ; j++){
+                if(sentences[i][j] == ' '){
+                    count++;
+                }
             }
+            count++;
+            maxium = max(maxium , count);
         }
-        count++;
-        maxiu = max(maxiu , count);
-       } 
-       return maxiu;
+
+        return maxium;
     }
 };

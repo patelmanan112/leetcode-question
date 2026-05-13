9bc3f8ec2f9efbc28ecbd4fb2feff11c6332ced3
Manan Patel <manan.patel.cg@gmail.com>
2026-05-13 23:25:34 +0530

    Time: 3 ms (4.77%), Space: 8.4 MB (46.97%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countAsterisks(string s) {
+        int count =0;
+        int check =0;
+        for(int i=0; i<s.size() ; i++){
+            if(check%2==0 ){
+               if(s[i] == '*') {
+                count++;
+               }
+            }
+            if(s[i] == '|'){
+                check++;
+            }
+        }
+
+        return count;
+    }
+};

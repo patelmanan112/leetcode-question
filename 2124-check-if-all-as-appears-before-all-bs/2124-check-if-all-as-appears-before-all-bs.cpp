a0b745f861e8128fd08bdb66ac4d0530960e69db
Manan Patel <manan.patel.cg@gmail.com>
2026-05-14 15:14:27 +0530

    Time: 0 ms (100%), Space: 8.1 MB (85.36%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     bool checkString(string s) {
-        int check = 0;
-
-        for(char ch : s) {
-            if(ch == 'b') {
-                check = 1;
+        int check =0;
+        for(char ch: s){
+            if(ch == 'b'){
+                check =1;
             }
-            else if(ch == 'a' && check) {
+            else if(ch == 'a' && check){
                 return false;
             }
         }
-
         return true;
     }
 };

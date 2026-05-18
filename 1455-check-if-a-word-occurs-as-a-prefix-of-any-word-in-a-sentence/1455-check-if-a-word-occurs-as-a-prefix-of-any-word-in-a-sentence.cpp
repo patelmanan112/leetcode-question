66c88cfcf7c751bd4e58905f450efddc9a591348
Manan Patel <manan.patel.cg@gmail.com>
2026-05-18 23:09:37 +0530

    Time: 0 ms (100%), Space: 8.1 MB (98.27%) - LeetHub

---
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+        string word = "";
+        int index = 1;
+
+        for (int i = 0; i <= sentence.size(); i++) {
+            if (i < sentence.size() && sentence[i] != ' ') {
+                word += sentence[i];
+            }
+            else {
+                if (word.find(searchWord) == 0) {
+                    return index;
+                }
+
+                word = "";
+                index++;
+            }
+        }
+
+        return -1;
+    }
+};

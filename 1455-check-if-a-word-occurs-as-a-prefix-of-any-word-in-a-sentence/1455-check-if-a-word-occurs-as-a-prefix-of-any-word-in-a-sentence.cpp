37805ea4d493114e0d6f42ac19ebed585e29d319
Manan Patel <manan.patel.cg@gmail.com>
2026-05-21 16:31:11 +0530

    Time: 0 ms (100%), Space: 8.2 MB (86.1%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     int isPrefixOfWord(string sentence, string searchWord) {
         string word = "";
-        int index = 1;
-
-        for (int i = 0; i <= sentence.size(); i++) {
-            if (i < sentence.size() && sentence[i] != ' ') {
-                word += sentence[i];
+        int index =1;
+        for(int i=0; i<sentence.size() ; i++){
+            if( i < sentence.size() && sentence[i] != ' '){
+                word+= sentence[i];
             }
-            else {
-                if (word.find(searchWord) == 0) {
+            else{
+                if(word.find(searchWord) == 0){
                     return index;
                 }
 
                 word = "";
                 index++;
             }
+        }
+        if(word.find(searchWord) ==0){
+            return index;
         }
 
         return -1;

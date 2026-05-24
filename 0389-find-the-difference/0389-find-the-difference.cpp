7dece4c672929b20e9f9c20ef6fa57bb24da1086
Manan Patel <manan.patel.cg@gmail.com>
2026-05-24 16:36:32 +0530

    Time: 0 ms (100%), Space: 9.5 MB (7.48%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -4,14 +4,12 @@ public:
         unordered_map<char , int> vp1;
         unordered_map<char , int> vp2;
 
-        for(int i=0; i<s.size() ; i++){
+        for(int i =0; i<s.size() ; i++){
             vp1[s[i]]++;
         }
-
-        for(int i=0; i<t.size() ; i++){
+        for(int i =0; i<t.size() ; i++){
             vp2[t[i]]++;
         }
-
 
         for(int i =0; i<t.size() ; i++){
             if(vp1[t[i]] != vp2[t[i]]){

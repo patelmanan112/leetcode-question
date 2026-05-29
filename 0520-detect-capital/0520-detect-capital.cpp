faa559f264e9deb2721276eaaa9ecfac527fbe89
Manan Patel <manan.patel.cg@gmail.com>
2026-05-29 13:27:12 +0530

    Time: 0 ms (100%), Space: 8.2 MB (16.91%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        int count =0;
-        char check;
+        int count = 0;
+        char cha;
         for(int i =0; i<word.size() ; i++){
-            if(word[i] >= 'A' && word[i] <= 'Z'){
-                check = word[i];
+            if( word[i] >= 'A'  && word[i] <= 'Z'){
                 count++;
+                cha = word[i];
             }
         }
 
-        if(count ==0){
+        if(count == word.size()){
             return true;
         }
-        if(count == 1 && check == word[0]){
+        if(count == 1 && cha == word[0]){
             return true;
         }
-        else if(count == word.size()){
+
+        else if( count ==0){
             return true;
         }
+
 
         return false;
     }

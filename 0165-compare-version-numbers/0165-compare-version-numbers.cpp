eca747b911208bc631848072f236c2ffe6788247
Manan Patel <manan.patel.cg@gmail.com>
2026-05-30 15:04:08 +0530

    Time: 0 ms (100%), Space: 8.4 MB (29.62%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+
+        int i = 0;
+        int k = 0;
+
+        while (i < version1.size() || k < version2.size()) {
+
+            long long value1 = 0;
+            long long value2 = 0;
+
+            while (i < version1.size() && version1[i] != '.') {
+                value1 = value1 * 10 + (version1[i] - '0');
+                i++;
+            }
+
+            while (k < version2.size() && version2[k] != '.') {
+                value2 = value2 * 10 + (version2[k] - '0');
+                k++;
+            }
+
+            if (value1 > value2)
+                return 1;
+
+            if (value1 < value2)
+                return -1;
+
+            i++;
+            k++; 
+        }
+
+        return 0;
+    }
+};

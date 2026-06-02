47a5bbcb7f65d8f60a2b95e766e990f858026bab
Manan Patel <manan.patel.cg@gmail.com>
2026-06-02 17:21:21 +0530

    Time: 0 ms (100%), Space: 30.4 MB (47.79%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.cpp
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -6,8 +6,9 @@ public:
         ans.push_back(first);
         ans.push_back(value);
         for(int i=1; i<encoded.size() ; i++){
-          int value2 =  ans[i]^ encoded[i];
+          int value2 =  value^ encoded[i];
           ans.push_back(value2);
+          value = value2;
         }
 
         return ans;

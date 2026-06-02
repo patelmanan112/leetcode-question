cf9db0d36694d37500234174b69b2ad71be7526a
Manan Patel <manan.patel.cg@gmail.com>
2026-06-02 17:16:23 +0530

    Time: 7 ms (2.27%), Space: 30.5 MB (20.43%) - LeetHub

---
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        vector<int> ans;
+        int value = encoded[0]^first;
+        ans.push_back(first);
+        ans.push_back(value);
+        for(int i=1; i<encoded.size() ; i++){
+          int value2 =  ans[i]^ encoded[i];
+          ans.push_back(value2);
+        }
+
+        return ans;
+    }
+};

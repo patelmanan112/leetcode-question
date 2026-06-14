1ec8678e647fc3139adb33ce8aa3cedaabd30c2e
Manan Patel <manan.patel.cg@gmail.com>
2026-06-14 22:08:22 +0530

    Time: 0 ms (100%), Space: 50.2 MB (79.37%) - LeetHub

--- 3668-restore-finishing-order/3668-restore-finishing-order.cpp
+++ 3668-restore-finishing-order/3668-restore-finishing-order.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
-         vector<int> mp;
-         for(int i=0; i<order.size() ; i++){
-            for(int j =0; j<friends.size() ; j++){
-                if(order[i] == friends[j]){
-                    mp.push_back(order[i]);
-                } 
+    vector<int> mp;
+    for(int i=0; i<order.size() ; i++){
+        for(int j =0; j<friends.size() ; j++){
+            if(order[i] == friends[j]){
+                mp.push_back(order[i]);
             }
-         }
-
-         return mp;
+        }
+    }
+    return mp;
     }
 };

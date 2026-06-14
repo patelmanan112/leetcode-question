160a2660a56b13e77cd90f16a9afaa5b9adee778
Manan Patel <manan.patel.cg@gmail.com>
2026-06-14 21:38:21 +0530

    Time: 4 ms (76.72%), Space: 12 MB (96.79%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        int count = 0;
-        string res = "";
-        for(char ch : s){
-            if(!res.empty() && res.back() == ch){
-                res.push_back(ch);
+    int count =0;
+    string res = "";
+    for(char ch : s){
+        if(!res.empty() && res.back() == ch){
+            res.push_back(ch);
+            count++;
+        }
+        else{
+            res.push_back(ch);
+            count =1;
+        }
+        if( k == count){
+            res.erase(res.end() - k , res.end());
+            if(!res.empty()){
+            char ch = res.back();
+            count =0;
+            for( int i =res.size() -1 ; i>=0 && res[i] == ch ; i--){
                 count++;
             }
-            else{
-                res.push_back(ch);
-                count = 1;
-            }
-            if(k == count){
-                res.erase(res.end()-k,res.end());
-                if(!res.empty()){
-                    char ch = res.back();
-                    count = 0;
-                    for(int i  = res.size()-1; i>=0 && res[i] == ch; i--){
-                        count++;
-                    }
-                }
             }
         }
-        return res;
+    }
+
+    return res;
     }
 };

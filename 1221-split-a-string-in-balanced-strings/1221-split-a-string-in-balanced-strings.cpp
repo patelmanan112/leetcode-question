412ec4bf8bc39f9410f788532b5f855eb53bb3a0
Manan Patel <manan.patel.cg@gmail.com>
2026-06-17 09:40:25 +0530

    Time: 0 ms (100%), Space: 8.2 MB (83.15%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int count =0;
+        int sum =0;
+        for(int i=0; i<s.size(); i++){
+          
+            if(s[i] == 'L'){
+                sum  = sum - 1;
+            }
+            else if(s[i] == 'R'){
+                sum = sum + 1;
+            }
+              if(sum == 0 ){
+                count++;
+            }
+        }
+        return count;
+    }
+};

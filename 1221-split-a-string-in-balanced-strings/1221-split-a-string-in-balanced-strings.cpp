1e290204d804599bb778cc54671c32732a1d665d
Manan Patel <manan.patel.cg@gmail.com>
2026-07-05 18:19:54 +0530

    Time: 0 ms (100%), Space: 8.5 MB (17.07%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int balancedStringSplit(string s) {
-        int count =0;
-        int sum =0;
-        for(int i=0; i<s.size(); i++){
-          
-            if(s[i] == 'L'){
-                sum  = sum - 1;
-            }
-            else if(s[i] == 'R'){
-                sum = sum + 1;
-            }
-              if(sum == 0 ){
-                count++;
-            }
+    int count =0;
+    int sum =0;
+    for(int i=0; i<s.size() ; i++){
+        if(s[i] == 'L'){
+            sum = sum -1;
         }
-        return count;
+        else if( s[i] == 'R'){
+            sum = sum + 1;
+        }
+        if(sum == 0){
+            count++;
+        }
+    }
+    return count;
     }
 };

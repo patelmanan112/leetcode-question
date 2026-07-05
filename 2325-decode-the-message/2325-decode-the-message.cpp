9abdbe30ee53a52ef34f41dcff6f96a88bcae980
Manan Patel <manan.patel.cg@gmail.com>
2026-07-05 17:29:01 +0530

    Time: 0 ms (100%), Space: 9.9 MB (28.23%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.cpp
+++ 2325-decode-the-message/2325-decode-the-message.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     string decodeMessage(string key, string message) {
-        unordered_map<char, char> mp;
-        char ch = 'a';
-
-        for (char c : key) {
-            if (c != ' ' && mp.find(c) == mp.end()) {
-                mp[c] = ch;
-                ch++;
-            }
+      unordered_map<char , char> mp;
+      char ch = 'a';
+      for(char c : key){
+        if(c != ' ' && mp.find(c) == mp.end()){
+            mp[c] = ch;
+            ch++;
         }
+      }
 
-        string ans;
-
-        for (char c : message) {
-            if (c == ' ')
-                ans += ' ';
-            else
-                ans += mp[c];
+      string ans;
+      for(char c : message){
+        if(c == ' '){
+            ans += ' ';
         }
-
-        return ans;
+        else{
+            ans += mp[c];
+        }
+      }
+      return ans;
     }
 };

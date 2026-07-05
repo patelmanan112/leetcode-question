5dde95b8cddb9c309bc0dc9cfcc019bdd16c28b1
Manan Patel <manan.patel.cg@gmail.com>
2026-07-05 17:35:48 +0530

    Time: 0 ms (100%), Space: 8.3 MB (55.21%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.cpp
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int countSegments(string s) {
-        int count =0;
-        int maxium =0;
-        for(int i=0; i<s.size(); i++){
-            if(s[i] != ' ' && i == s.size() -1){
-                count++;
-                break;
-            }
-            if(s[i] != ' ' && s[i +1] == ' '){
-                 count++ ;
-            }
-            else{
-               continue;
-            }
+     int count =0;
+   for(int i=0; i<s.size(); i++){
+    if(s[i] != ' ' && i == s.size() -1){
+        count++;
+        break;
+    }
+    if(s[i] != ' ' && s[i+1] == ' '){
+        count++;
+    }
+    else{
+        continue;
+    }
+   }
 
-              
-        }
-
-       
-        return count;
+   return count;
     }
 };

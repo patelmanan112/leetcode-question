48f0017db0fc31b4ced67e64cf09374580dce6bc
Manan Patel <manan.patel.cg@gmail.com>
2026-07-28 16:49:57 +0530

    Time: 420 ms (5.03%), Space: 19.7 MB (29.53%) - LeetHub

---
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int count =0;
+        for(int i =0; i<nums.size() -1 ; i++){
+            while(!(nums[i] < nums[i+1])){
+                nums[i+1] = nums[i+1] + 1;
+                count++;
+            }
+        }
+        return count;
+    }
+};

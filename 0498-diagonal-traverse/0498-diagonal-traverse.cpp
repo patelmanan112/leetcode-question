2cf2c3ed8f869767e5ecfff3db4e798067304961
Manan Patel <manan.patel.cg@gmail.com>
2026-08-25 15:35:42 +0530

    Time: 37 ms (10.28%), Space: 31.1 MB (18.76%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
-        map<int , vector<int>> freq;
-        for(int i =0; i < mat.size(); i++){
-            for(int j =0; j<mat[i].size(); j++){
+        map<int, vector<int>> freq;
+        for (int i = 0; i < mat.size(); i++) {
+            for (int j = 0; j < mat[i].size(); j++) {
                 freq[i + j].push_back(mat[i][j]);
             }
         }
         vector<int> ans;
-        for(auto& it : freq){
-            if(it.first%2 !=0)
-           { for(auto&sec :it.second ){
-                ans.push_back(sec);
-            }}
-        
-        else{
-            for(int i = it.second.size() -1 ; i >=0; i--){
-                 ans.push_back(it.second[i]);
+        bool lowtohigh=true;
+        for (auto& it : freq) {
+            if (!lowtohigh) {
+                for (auto& sec : it.second) {
+                    ans.push_back(sec);
+                }
+                
             }
+
+            else {
+                for (int i = it.second.size() - 1; i >= 0; i--) {
+                    ans.push_back(it.second[i]);
+                }
+            }
+            lowtohigh=!lowtohigh;
         }
-        }
-        return ans;   
+        return ans;
     }
 };

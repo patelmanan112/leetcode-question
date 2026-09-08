dcb206672d24ac46f9f5afe51f117e7d67575ada
Manan Patel <manan.patel.cg@gmail.com>
2026-09-08 12:00:08 +0530

    Time: 12 ms (86.22%), Space: 29.9 MB (23.67%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -2,12 +2,28 @@ class Solution {
 public:
     string removeStars(string s) {
 
-        for(int i = 0; i<s.size() ; i++){
-            if(s[i] == '*'){
-                s.erase(i - 1  , 2);
-                i = i-2;
+        // for(int i = 0; i<s.size() ; i++){
+        //     if(s[i] == '*'){
+        //         s.erase(i - 1  , 2);
+        //         i = i-2;
+        //     }
+        // }
+        // return s;
+        string ans;
+        stack<char> sta;
+        for(char it : s){
+            if(it == '*'){
+                sta.pop();
+            }
+            else{
+                sta.push(it);
             }
         }
-        return s;
+        while(!sta.empty()){
+            ans +=  sta.top();
+            sta.pop();
+        }
+        reverse(ans.begin() , ans.end());
+        return ans;
     }
 };

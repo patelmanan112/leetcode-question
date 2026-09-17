180befc3fc743f5a19f09c3dc00d222815147dee
Manan Patel <manan.patel.cg@gmail.com>
2026-09-17 10:33:01 +0530

    Time: 0 ms (100%), Space: 15.5 MB (88.69%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -1,22 +1,62 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if (head == nullptr || head->next == nullptr)
+        // if (head == nullptr || head->next == nullptr)
+        //     return head;
+
+        // ListNode* odd = head;
+        // ListNode* even = head->next;
+        // ListNode* evenHead = even;
+
+        // while (even != nullptr && even->next != nullptr) {
+        //     odd->next = even->next;
+        //     odd = odd->next;
+
+        //     even->next = odd->next;
+        //     even = even->next;
+        // }
+
+        // odd->next = evenHead;
+        // return head;
+
+// Second Logic 
+        // if(head == nullptr || head->next == nullptr){
+        //     return head;
+        // }
+
+        // ListNode* slow = head;
+        // ListNode* fast = head->next;
+
+        // while(fast != nullptr && fast->next != nullptr){
+        //     ListNode* curr = slow->next;
+        //     ListNode* nex = fast->next;
+        //     slow->next = nex;
+        //     fast->next = curr;
+        //     slow = slow->next;
+        //     fast = fast->next;
+        // }
+        // return head;
+
+
+        if(head == nullptr || head->next == nullptr){
             return head;
 
-        ListNode* odd = head;
-        ListNode* even = head->next;
-        ListNode* evenHead = even;
-
-        while (even != nullptr && even->next != nullptr) {
-            odd->next = even->next;
-            odd = odd->next;
-
-            even->next = odd->next;
-            even = even->next;
         }
+        ListNode* i = head;
+        ListNode* j = head->next;
+        ListNode* temp = j;
 
-        odd->next = evenHead;
+        while(j != nullptr && j->next != nullptr){
+            i->next = i->next->next;
+            i = i->next;
+            j->next = j->next->next;
+            j = j->next;
+
+        }
+        i->next = temp;
+        if(j !=nullptr){
+            j->next = nullptr;
+        }
         return head;
     }
 };
